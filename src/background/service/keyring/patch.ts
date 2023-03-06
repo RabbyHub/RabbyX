c26f2032e80bf55554afdb07a7266a60c9482018
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2023-03-06 13:34:07 +0800

    chore: try to fix patch-package. (#20)

--- src/background/service/keyring/patch.ts
+++ src/background/service/keyring/patch.ts
@@ -1,6 +1,6 @@
 
 // patch trezor-like
-import TrezorConnect from 'trezor-connect';
+import TrezorConnect from '@trezor/connect';
 import OneKeyConnect from '@onekeyfe/connect';
 
 (globalThis as any)._TrezorConnect = TrezorConnect;
