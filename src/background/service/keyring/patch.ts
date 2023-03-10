4c8f18915667b74f53720ed31750cd49f225e748
heisenberg <110591045+heisenberg-2077@users.noreply.github.com>
2023-03-10 15:23:01 +0800

    feat: upgrade trezor

--- src/background/service/keyring/patch.ts
+++ src/background/service/keyring/patch.ts
@@ -1,5 +1,5 @@
 // patch trezor-like
-import TrezorConnect from 'trezor-connect';
+import TrezorConnect from '@trezor/connect';
 import OneKeyConnect from '@onekeyfe/connect';
 
 (globalThis as any)._TrezorConnect = TrezorConnect;
