37122c925caa6c6ba7143e8484efc7aa4f4a7e30
qingwei-li <heisenberg-2077@outlook.com>
2023-05-31 23:41:36 +0800

    fix: remove console

--- src/utils/ledger.ts
+++ src/utils/ledger.ts
@@ -63,7 +63,6 @@ export const useLedgerDeviceConnected = () => {
     fetchDevices();
 
     setTimeout(() => {
-      console.log('loopFetchDevices');
       loopFetchDevices();
     }, 500);
   };
