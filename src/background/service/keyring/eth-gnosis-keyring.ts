eec74d38a81ba22ff48c234c837d9db97813b896
richardo2016x <richardo2016x@gmail.com>
2023-06-14 20:21:34 +0800

    Revert "fix: gnosis network error alert (#1447)"
    This reverts commit bee80c193ba0a688b731ff1dd735bfba2a40f3f8.

--- src/background/service/keyring/eth-gnosis-keyring.ts
+++ src/background/service/keyring/eth-gnosis-keyring.ts
@@ -387,6 +387,10 @@ class GnosisKeyring extends EventEmitter {
   }
 
   async getOwners(address: string, version: string, provider, networkId) {
+    const networkIds = this.networkIdsMap[address.toLowerCase()];
+    if (!networkId || !networkIds || !networkIds.includes(networkId)) {
+      throw new Error(`No networkId in keyring for address ${address}`);
+    }
     const safe = new Safe(address, version, provider, networkId);
     const owners = await safe.getOwners();
     return owners;
