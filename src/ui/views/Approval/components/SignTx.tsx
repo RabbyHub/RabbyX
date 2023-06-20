b3896d5305783d65b709971a0ef13711f01a6923
richardo2016x <richardo2016x@gmail.com>
2023-06-20 14:44:39 +0800

    Revert "fix: gnosis network error alert (#1447)"
    This reverts commit bee80c193ba0a688b731ff1dd735bfba2a40f3f8.

--- src/ui/views/Approval/components/SignTx.tsx
+++ src/ui/views/Approval/components/SignTx.tsx
@@ -1406,25 +1406,8 @@ const SignTx = ({ params, origin }: SignTxProps) => {
 
   const getSafeInfo = async () => {
     const currentAccount = (await wallet.getCurrentAccount())!;
-    const networkId = '' + chainId;
-    let safeInfo: SafeInfo | null = null;
-    try {
-      safeInfo = await Safe.getSafeInfo(currentAccount.address, networkId);
-    } catch (e) {
-      let networkIds: string[] = [];
-      try {
-        networkIds = await wallet.getGnosisNetworkIds(currentAccount.address);
-      } catch (e) {
-        console.error(e);
-      }
-      if (!networkIds.includes(networkId)) {
-        throw new Error(
-          `Current safe address is not supported on ${chain.name} chain`
-        );
-      } else {
-        throw e;
-      }
-    }
+    const networkId = String(chainId);
+    const safeInfo = await Safe.getSafeInfo(currentAccount.address, networkId);
     const pendingTxs = await Safe.getPendingTransactions(
       currentAccount.address,
       networkId
