5c2231da8743e519122d28749448191176454adc
richardo2016x <richardo2016x@gmail.com>
2023-06-28 09:59:13 +0800

    chore: adapt to desktop.

--- src/background/service/notification.ts
+++ src/background/service/notification.ts
@@ -359,7 +359,11 @@ class NotificationService extends Events {
           focused: true,
         });
       } else {
-        this.openNotification(approval.winProps);
+        this.openNotification(
+          approval.winProps,
+          false,
+          approval.data.approvalComponent
+        );
       }
     });
   };
@@ -398,7 +402,7 @@ class NotificationService extends Events {
     this.isLocked = true;
   };
 
-  openNotification = (winProps, ignoreLock = false) => {
+  openNotification = (winProps, ignoreLock = false, approvalType?: string) => {
     // Only use ignoreLock flag when approval exist but no notification window exist
     if (!ignoreLock) {
       if (this.isLocked) return;
@@ -408,6 +412,10 @@ class NotificationService extends Events {
       winMgr.remove(this.notifiWindowId);
       this.notifiWindowId = null;
     }
+    if (approvalType) {
+      winProps.query = `type=${approvalType}`;
+    }
+
     winMgr.openNotification(winProps).then((winId) => {
       this.notifiWindowId = winId!;
     });
