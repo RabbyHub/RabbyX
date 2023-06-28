34301164076d1328bf5edb7131695fc397762ead
richardo2016x <richardo2016x@gmail.com>
2023-06-28 09:59:13 +0800

    chore: adapt to desktop.

--- src/ui/views/Approval/components/SignTx.tsx
+++ src/ui/views/Approval/components/SignTx.tsx
@@ -90,6 +90,7 @@ interface BasicCoboArgusInfo {
   networkId: string;
   delegates: string[];
 }
+import { genMintRabbyTxDetail } from '@/constant/mint-rabby/gen-tx-detail';
 
 const normalizeHex = (value: string | number) => {
   if (typeof value === 'number') {
@@ -157,9 +158,9 @@ export const TxTypeComponent = ({
   onChange,
   isSpeedUp,
   engineResults,
-  txDetail,
   origin,
   originLogo,
+  txDetail: oldTxDetail,
 }: {
   actionRequireData: ActionRequireData;
   actionData: ParsedActionData;
@@ -174,6 +175,11 @@ export const TxTypeComponent = ({
   originLogo?: string;
 }) => {
   if (!isReady) return <Loading />;
+
+  const txDetail = useMemo(() => genMintRabbyTxDetail(oldTxDetail), [
+    oldTxDetail,
+  ]);
+
   if (actionData && actionRequireData) {
     return (
       <Actions
