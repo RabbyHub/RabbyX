fb04adb175ee51943c738f1f4f0bf8124886930c
richardo2016x <richardo2016x@gmail.com>
2023-06-28 09:59:13 +0800

    chore: adapt to desktop.

--- src/background/controller/provider/controller.ts
+++ src/background/controller/provider/controller.ts
@@ -513,6 +513,13 @@ class ProviderController extends BaseController {
           swapService.postSwap(chain, hash, other);
           bridgeService.postBridge(chain, hash, other);
         }
+        sessionService.broadcastToDesktopOnly('transactionChanged', {
+          type: 'push-tx',
+          ...other,
+          value: approvalRes.value || '0x0',
+          hash: hash,
+          chain,
+        });
 
         statsData.submit = true;
         statsData.submitSuccess = true;
@@ -756,7 +763,19 @@ class ProviderController extends BaseController {
       if (notificationService.statsData?.signMethod) {
         statsData.signMethod = notificationService.statsData?.signMethod;
       }
+      // transactionHistoryService.removeSigningTx(signingTxId!);
       notificationService.setStatsData(statsData);
+
+      const errMsg = e.message || JSON.stringify(e);
+      // notification.create(
+      //   undefined,
+      //   i18n.t('background.error.txPushFailed'),
+      //   errMsg
+      // );
+      sessionService.broadcastToDesktopOnly('transactionChanged', {
+        type: 'push-failed',
+        errMsg,
+      });
       throw typeof e === 'object' ? e : new Error(e);
     }
   };
@@ -770,7 +789,7 @@ class ProviderController extends BaseController {
 
   @Reflect.metadata('SAFE', true)
   web3ClientVersion = () => {
-    return `Rabby/${process.env.release}`;
+    return `RabbyX/${globalThis.rabbyDesktop.appVersion}`;
   };
 
   @Reflect.metadata('APPROVAL', ['ETHSign', () => null, { height: 390 }])
@@ -1001,7 +1020,7 @@ class ProviderController extends BaseController {
   };
 
   @Reflect.metadata('APPROVAL', [
-    'AddChain',
+    'SwitchChain',
     ({
       data: {
         params: [chainParams],
--- src/background/controller/provider/rpcFlow.ts
+++ src/background/controller/provider/rpcFlow.ts
@@ -121,7 +121,7 @@ const flowContext = flow
               params: { origin, name, icon },
               approvalComponent: 'Connect',
             },
-            { height: 800 }
+            { height: 390 }
           );
           connectOrigins.delete(origin);
           permissionService.addConnectedSiteV2({
